0bd649af1ee741d54fd715eb3ef8fba5b92ca25c
hosam7asko1993@gmail.com <hosam7asko1993@gmail.com>
2020-05-21 23:36:16 +0530

    add date format

---
+++ ed-rules-api/src/main/java/com/hit/health/config/ConvertDate.java
@@ -0,0 +1,36 @@
+package com.hit.health.config;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ConvertDate {
+
+	private String today;
+	private String nextYear;
+	public ConvertDate() {
+		SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/yyyy");
+		Calendar cal = Calendar.getInstance();
+		Date todayDate = cal.getTime();
+		cal.add(Calendar.YEAR, 1);
+		Date nextYear = cal.getTime();
+		this.today=dateFormat.format(todayDate);
+		this.nextYear=dateFormat.format(nextYear);
+	}
+	public String getToday() {
+		return today;
+	}
+	public void setToday(String today) {
+		this.today = today;
+	}
+	public String getNextYear() {
+		return nextYear;
+	}
+	public void setNextYear(String nextYear) {
+		this.nextYear = nextYear;
+	}
+	
+	
+
+	
+}
